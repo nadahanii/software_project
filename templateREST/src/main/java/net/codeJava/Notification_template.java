b30d24a7d842b9877837b163235bc78a5e53500d
nadahanii <66536961+nadahanii@users.noreply.github.com>
2021-01-06 16:11:37 +0200

    deleted unused folders

--- templateREST/src/main/java/net/codeJava/Notification_template.java
+++
@@ -1,7 +0,0 @@
-package net.codeJava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Notification_template extends JpaRepository<template, Integer> {
-	 
-}
--- templateRestwith sending queues/templateREST/src/main/java/net/codeJava/senderRepo.java
+++
@@ -1,7 +0,0 @@
-package net.codeJava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface senderRepo extends JpaRepository<sendingqueue, Integer> {
-	
-}
