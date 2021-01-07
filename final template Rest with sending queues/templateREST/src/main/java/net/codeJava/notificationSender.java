03d27edb47b752fd36a0a822c6b6233d70173319
habiba mohamed <bobah1653@gmail.com>
2021-01-07 14:47:39 +0200

    Update notificationSender.java

--- final template Rest with sending queues/templateREST/src/main/java/net/codeJava/notificationSender.java
+++ final template Rest with sending queues/templateREST/src/main/java/net/codeJava/notificationSender.java
@@ -1,8 +1,15 @@
 package net.codeJava;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,37 +18,83 @@ public class notificationSender {
 	private Notifications notificationService;
 	@Autowired
 	private senderService senderService;
-	 @GetMapping ("/send/{subject}/{name}")
-	public void send_notification(@PathVariable String subject,@PathVariable String name) {
-		 template temp=notificationService.findBySub(subject);
+	 @GetMapping ("/send/{id}/{method}/{name}")
+	 public void send_notification(@PathVariable int id,@PathVariable String method,@PathVariable String name,@RequestBody ArrayList<String> placeholders)
+	 {
+		 sendingqueue toBeSent = new sendingqueue();
+		 template temp = notificationService.get(id);
 		 String tempContent = temp.getContent();
+		 String methood = method;
 		 String res="";
-		 String methood="";
-		 for(int i=0;i<tempContent.length();i++) {
-			 if(tempContent.charAt(i)=='(' && tempContent.charAt(i+1)=='n' && tempContent.charAt(i+2)==')') {
-				 res+=name;
+		 int c=0 , j=0;
+		 for(int i=0;i<tempContent.length();i++)
+		 {
+			 if(tempContent.charAt(i)=='('  && placeholders.size()!=0)
+			 {
+				 res+=placeholders.get(j);
+				 j++;
+				
 				 i+=3;
-				 methood="mail";
+				 c++;
+				 
 			 }
-			 else if(tempContent.charAt(i)=='(' && tempContent.charAt(i+1)=='p' && tempContent.charAt(i+2)==')') {
-				 res+=name;
-				 i+=3;
-				 methood="SMS";
-			 }
+			 
 			 else
+			 {
 				 res+=tempContent.charAt(i);
+			 }
 		 }
 		 
-		 sendingqueue toBeSent = new sendingqueue();
-		 toBeSent.setMethod(methood);
-		 toBeSent.setcontent(res);
 		 
+		 
+		 
+		 if(c!=placeholders.size())
+		 {
+			 toBeSent.setStatus("failed");
+			 
+		 }
+		 
+		 else
+		 {
+			 if((methood.equalsIgnoreCase("SMS") && name.length()==11) || (methood.equalsIgnoreCase("email") && name.contains("@") && name.contains(".com")))
+			 {
+				 toBeSent.setStatus("waiting");
+			 }
+			 
+			 else
+			 {
+				 toBeSent.setStatus("failed");
+			 }
+			 
+		 }
+		 
+		 
+		 
+		 
+		 toBeSent.setReceiver(name);
+		 toBeSent.setcontent(res);
+		 toBeSent.setMethod(methood);
 		 senderService.save(toBeSent);
 		 
-		 
-		 
-		 
 	 }
+	 
+	 
+	 @DeleteMapping("/sending/{id}")
+	 public ResponseEntity<?> delete(@PathVariable Integer id)
+	 {
+		 try
+		 {
+			sendingqueue temp = senderService.get(id);
+		 senderService.delete(id);
+		 return new ResponseEntity<>(HttpStatus.OK);
+		 }catch(NoSuchElementException e)
+		 {
+			 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		 }
+	 }
+	 
+	 
+	
 	
 
 }
